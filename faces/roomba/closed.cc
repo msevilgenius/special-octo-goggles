c5941daa75822a95b7ba813693fd9fed5aa54057
Michael Sledge <msevilgenius@users.noreply.github.com>
2018-10-14 21:29:22 +0100

    fix missing drawing namespace

--- faces/roomba/closed.cc
+++ faces/roomba/closed.cc
@@ -16,8 +16,8 @@ Closed::Closed(Color color) : _color(color)
 
 void Closed::Render(FrameCanvas* canvas)
 {
-    DrawRect(canvas,  3, 8, 9, 2, _color);
-    DrawRect(canvas, 20, 8, 9, 2, _color);
+    drawing::DrawRect(canvas,  3, 8, 9, 2, _color);
+    drawing::DrawRect(canvas, 20, 8, 9, 2, _color);
 }
 
 }
--- faces/roomba/normal.cc
+++ faces/roomba/normal.cc
@@ -15,10 +15,10 @@ Normal::Normal(Color color) : _color(color)
 
 void Normal::Render(FrameCanvas* canvas)
 {
-    DrawRect(canvas, 6, 5, 3, 6, _color)
+    drawing::DrawRect(canvas, 6, 5, 3, 6, _color)
     canvas->SetPixel( 7,  4, _color.r, _color.g, _color.b);
     canvas->SetPixel( 7, 11, _color.r, _color.g, _color.b);
-    DrawRect(canvas, 23, 5, 3, 6, _color)
+    drawing::DrawRect(canvas, 23, 5, 3, 6, _color)
     canvas->SetPixel(24,  4, _color.r, _color.g, _color.b);
     canvas->SetPixel(24, 11, _color.r, _color.g, _color.b);
 }
