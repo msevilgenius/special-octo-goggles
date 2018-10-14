5bade20a129bd3d7257bc6a3cb01162a1cb78c69
Michael Sledge <msevilgenius@users.noreply.github.com>
2018-10-14 21:43:53 +0100

    'default' implementation for interfaces so it links properly

--- faces/faces.h
+++ faces/faces.h
@@ -9,14 +9,14 @@ namespace faces{
 class IFace {
     public:
         virtual ~IFace() {}
-        virtual void Start();
-        virtual void Update(const Uint32 time);
-        virtual void OnEvent(SDL_Event* event);
+        virtual void Start() {}
+        virtual void Update(const Uint32 time) {}
+        virtual void OnEvent(SDL_Event* event) {}
 };
 
 class IFaceState {
     public:
-        virtual void Render(rgb_matrix::FrameCanvas* canvas);
+        virtual void Render(rgb_matrix::FrameCanvas* canvas) {}
 };
 
 
